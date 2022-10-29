05ebf344d9ca29afd1ad08441509eccf9e4d1685
Fatima <bossfatya@gmail.com>
2022-10-29 16:07:10 +0400

    switch added

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int new_int = 2;
+            double newDouble = 2.3;
+            char newChar = 'a';
+            string N4me = "Fatima";
+            bool newBool = false;
+            float newFloat = 2.2f;
+            byte new_byte = 8;
+
+            Console.WriteLine(new_int);
+            Console.WriteLine(newDouble);
+            Console.WriteLine(N4me);
+            Console.WriteLine(newChar);
+            Console.WriteLine(newBool);
+            Console.WriteLine(newFloat);
+            Console.WriteLine(new_byte);
+          
+            string a = ConsoleRaadLine();
+            Console.WriteLine($"gonderdiyimiz deyer{int_a}");
+              */
+            //int a = 2;
+            //int b = 8;
+            //int c = a + b;
+            // Console.ReadLine(int c); //
+            //Console.WriteLine(c);//
+
+            //string A = Console.ReadLine();
+            //string B = Console.ReadLine();
+            //int C = Int32.Parse(A) + Int32.Parse(B);
+            //Console.WriteLine(C);
+            // ==(VALUE) OLANDA REQEMLERI YOXLAYIR ===(VALUE AND TYPE) OLANDA ISE HEM REQEM HEM NOVUNU YENI STRING VE INT =5 OLSA FALSE OLUR
+            // !==  HEM VALUE HEM TIP BERABER DEYIL != BERABER DEYIL AMA TIPE BAXMIR
+            //
+
+        }
+    }
+}
